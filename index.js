44fa73b77fe55a89936ecec477c49b55e6ec5f1c
Jennifer Man <jenniferman702@gmail.com>
2022-09-23 19:17:07 -0400

    commit tree 36fabb5ef042b76f7b5129b5fafbefe2c9cc55fd

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName(customerName){
+    let result = customerName.toUpperCase();
+    return result;
+}
+
+function setBestCustomer(bestCustomer){
+    let bestCustomer = 'not bob';
+    return setBestCustomer;
+} 
